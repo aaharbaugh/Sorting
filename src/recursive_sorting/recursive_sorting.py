4f7eef0f076c9e769cae5070b7f51a92cfd96d9b
aaharbaugh <aaronmharbaugh@gmail.com>
2019-09-10 17:00:14 -0500

    finished the merge helper function on recursive sorting, very complex if statement that needs refactoring

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,12 +1,31 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
+
+arrA = [1, 4, 5, 6]
+arrB = [2, 3, 9, 10]
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
+
+    for i in range(len(merged_arr)):
+        if len(arrA) != 0 and len(arrB) != 0:
+            if arrA[0] <= arrB[0]:
+                merged_arr[i] = arrA[0]
+                arrA.pop(0)
+            else:
+                merged_arr[i] = arrB[0]
+                arrB.pop(0)
+        else:
+            if len(arrA) != 0:
+                merged_arr[i] = arrA[0]
+                arrA.pop(0)
+            if len(arrB) != 0:
+                merged_arr[i] = arrB[0]
+                arrB.pop(0)
     
     return merged_arr
 
-
+print(merge(arrA, arrB))
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
